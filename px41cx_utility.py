6c000a7aaee3c28a802028f06876dc4ab6199646
diemheych <100515426+diemheych@users.noreply.github.com>
2024-09-20 23:10:06 +1000

    New options
    Show firmware version and date language with firmware information.
    Add options to set date language.

--- px41cx_utility.py
+++ px41cx_utility.py
@@ -21,6 +21,7 @@
 #                          [-u3 "custom string line 3"}
 #                          [-u4 "custom string line 4"]
 #                          [-b BMPFILE]
+#                          [-eng | -fre | -spa | -ger | -ita | -por]
 #                          infile [outfile]
 #
 #
@@ -47,7 +48,7 @@
 # 
 # This program is free software: you can redistribute it and/or modify it under the
 # terms of the GNU General Public License as published by the Free Software Foundation,
-# version 3 of the License.
+# either version 3 of the License, or (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
@@ -78,6 +79,25 @@ MAGIC2 = 0x94
 IMAGE_SIZE = 2048
 WIDTH = 250
 HEIGHT = 122
+FW_VERSION = b'VER: '
+DATE_ENG = b'JAN\x00FEB\x00MAR\x00APR\x00MAY\x00JUN\x00JUL\x00AUG\x00SEP\x00OCT\x00NOV\x00DEC\x00SUN\x00MON\x00TUE\x00WED\x00THU\x00FRI\x00SAT\x00'
+DAY_ENG = b'SUN\x00MON\x00TUE\x00WED\x00THU\x00FRI\x00SAT\x00'
+MONTH_ENG = b'JAN\x00FEB\x00MAR\x00APR\x00MAY\x00JUN\x00JUL\x00AUG\x00SEP\x00OCT\x00NOV\x00DEC\x00'
+DATE_FRE = b'JAN\x00FEV\x00MAR\x00AVR\x00MAI\x00JUN\x00JUL\x00AOU\x00SEP\x00OCT\x00NOV\x00DEC\x00DIM\x00LUN\x00MAR\x00MER\x00JEU\x00VEN\x00SAM\x00'
+DAY_FRE = b'DIM\x00LUN\x00MAR\x00MER\x00JEU\x00VEN\x00SAM\x00'
+MONTH_FRE = b'JAN\x00FEV\x00MAR\x00AVR\x00MAI\x00JUN\x00JUL\x00AOU\x00SEP\x00OCT\x00NOV\x00DEC\x00'
+DATE_SPA = b'ENE\x00FEB\x00MAR\x00ABR\x00MAY\x00JUN\x00JUL\x00AGO\x00SEP\x00OCT\x00NOV\x00DEC\x00DOM\x00LUN\x00MAR\x00MIE\x00JUE\x00VIE\x00SAB\x00'
+DAY_SPA = b'DOM\x00LUN\x00MAR\x00MIE\x00JUE\x00VIE\x00SAB\x00'
+MONTH_SPA = b'ENE\x00FEB\x00MAR\x00ABR\x00MAY\x00JUN\x00JUL\x00AGO\x00SEP\x00OCT\x00NOV\x00DEC\x00'
+DATE_GER = b'JAN\x00FEB\x00MAR\x00APR\x00MAI\x00JUN\x00JUL\x00AUG\x00SEP\x00OKT\x00NOV\x00DEZ\x00SON\x00MON\x00DIE\x00MIT\x00DON\x00FRE\x00SAM\x00'
+DAY_GER = b'SON\x00MON\x00DIE\x00MIT\x00DON\x00FRE\x00SAM\x00'
+MONTH_GER = b'JAN\x00FEB\x00MAR\x00APR\x00MAI\x00JUN\x00JUL\x00AUG\x00SEP\x00OKT\x00NOV\x00DEZ\x00'
+DATE_ITA = b'GEN\x00FEB\x00MAR\x00APR\x00MAG\x00GIU\x00LUG\x00AGO\x00SET\x00OTT\x00NOV\x00DIC\x00DOM\x00LUN\x00MAR\x00MER\x00GIO\x00VEN\x00SAB\x00'
+DAY_ITA = b'DOM\x00LUN\x00MAR\x00MER\x00GIO\x00VEN\x00SAB\x00'
+MONTH_ITA = b'GEN\x00FEB\x00MAR\x00APR\x00MAG\x00GIU\x00LUG\x00AGO\x00SET\x00OTT\x00NOV\x00DIC\x00'
+DATE_POR = b'JAN\x00FEV\x00MAR\x00ABR\x00MAI\x00JUN\x00JUL\x00AGO\x00SET\x00OCT\x00NOV\x00DEZ\x00DOM\x00SEG\x00TER\x00QUA\x00QUI\x00SEX\x00SAB\x00'
+DAY_POR = b'DOM\x00SEG\x00TER\x00QUA\x00QUI\x00SEX\x00SAB\x00'
+MONTH_POR = b'JAN\x00FEV\x00MAR\x00ABR\x00MAI\x00JUN\x00JUL\x00AGO\x00SET\x00OCT\x00NOV\x00DEZ\x00'
 
 def set_rom(hex, rom, page, bank, bankgroup, modgroup):
     hex[ROM_MAP + ROM_MAP_ENTRY * rom + 0] = page
@@ -155,6 +175,14 @@ parser.add_argument('-u2',dest='user2',type=str,help="Info menu configurable tex
 parser.add_argument('-u3',dest='user3',type=str,help="Info menu configurable text line 3")
 parser.add_argument('-u4',dest='user4',type=str,help="Info menu configurable text line 4")
 parser.add_argument('-b',dest='bmpfile',type=str,help="BMP file for splash screen" )
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-eng", action="store_true",help="Set date strings to English")
+group.add_argument("-fre", action="store_true",help="Set date strings to French")
+group.add_argument("-spa", action="store_true",help="Set date strings to Spanish")
+group.add_argument("-ger", action="store_true",help="Set date strings to German")
+group.add_argument("-ita", action="store_true",help="Set date strings to Italian")
+group.add_argument("-por", action="store_true",help="Set date strings to Portuguese")
+
 
 args = vars(parser.parse_args())
 arg_count = len(args)
@@ -172,6 +200,9 @@ if ih[0] != MAGIC1 and ih[1] != MAGIC2:
 
 if (arg_count - list(args.values()).count(None) == 1) or (not args['outfile'] and not args['bmpfile']):
     print("PX41CX Firmware:",args['infile'])
+    if (version := ih.find(FW_VERSION)) != -1:
+        print(ih.getsz(version).decode('utf-8'))
+
     for a in range(ROM_MAP_SIZE):
         page, bank, group, modgroup = get_rom(ih, a)
         if page != 255:
@@ -192,6 +223,33 @@ if (arg_count - list(args.values()).count(None) == 1) or (not args['outfile'] an
         print("User 4: '",ih.getsz(USER4).decode('unicode_escape'),"'",sep="")
     else:
         print("User 4: '",repr(ih.getsz(USER4))[2:-1],"'",sep="")
+    if ih.find(DATE_ENG) != -1:
+        date_fmt = "English"
+    elif ih.find(DATE_FRE) != -1:
+        date_fmt = "French"
+    elif ih.find(DATE_SPA) != -1:
+        date_fmt = "Spanish"
+    elif ih.find(DATE_GER) != -1:
+        date_fmt = "German"
+    elif ih.find(DATE_ITA) != -1:
+        date_fmt = "Italian"
+    elif ih.find(DATE_POR) != -1:
+        date_fmt = "Portuguese"
+    elif ih.find(DAY_ENG) != -1:
+        date_fmt = "English"
+    elif ih.find(DAY_FRE) != -1:
+        date_fmt = "French"
+    elif ih.find(DAY_SPA) != -1:
+        date_fmt = "Spanish"
+    elif ih.find(DAY_GER) != -1:
+        date_fmt = "German"
+    elif ih.find(DAY_ITA) != -1:
+        date_fmt = "Italian"
+    elif ih.find(DAY_POR) != -1:
+        date_fmt = "Portuguese"
+    else:
+        date_fmt = "Unknown"
+    print("Date Format: ",date_fmt)
     exit(0)
 
 newrom = IntelHex()
@@ -265,8 +323,100 @@ if args['user4'] != None:
     ih.putsz(USER4, "{:31.31}".format(info))
     changed = True
 
-if args['bmpfile']:
+if args['eng'] or args['fre'] or args['spa'] or args['ger'] or args['ita'] or args['por']:
+    fw903plus = False
+    lang = 0
 
+    if (date_addr := ih.find(DATE_ENG)) != -1:
+        fw903plus = True
+        lang = 1
+    elif (date_addr := ih.find(DATE_FRE)) != -1:
+        fw903plus = True
+        lang = 2
+    elif (date_addr := ih.find(DATE_SPA)) != -1:
+        fw903plus = True
+        lang = 3
+    elif (date_addr := ih.find(DATE_GER)) != -1:
+        fw903plus = True
+        lang = 4
+    elif (date_addr := ih.find(DATE_ITA)) != -1:
+        fw903plus = True
+        lang = 5
+    elif (date_addr := ih.find(DATE_POR)) != -1:
+        fw903plus = True
+        lang = 6
+    elif (date_addr := ih.find(DAY_ENG)) != -1:
+        lang = 1
+    elif (date_addr := ih.find(DAY_FRE)) != -1:
+        lang = 2
+    elif (date_addr := ih.find(DAY_SPA)) != -1:
+        lang = 3
+    elif (date_addr := ih.find(DAY_GER)) != -1:
+        lang = 4
+    elif (date_addr := ih.find(DAY_ITA)) != -1:
+        lang = 5
+    elif (date_addr := ih.find(DAY_POR)) != -1:
+        lang = 6
+    else:
+        pass
+
+    if args['eng'] and lang:
+        print("Set language: English")
+        if fw903plus:
+            ih.puts(date_addr, DATE_ENG)
+            changed = True
+        elif (month_addr := ih.find(MONTH_ENG)) != -1:
+            ih.puts(date_addr, DAY_ENG)
+            changed = True
+
+    if args['fre'] and lang:
+        print("Set language: French")
+        if fw903plus:
+            ih.puts(date_addr, DATE_FRE)
+            changed = True
+        elif (month_addr := ih.find(MONTH_FRE)) != -1:
+            ih.puts(date_addr, DAY_FRE)
+            changed = True
+
+    if args['spa'] and lang:
+        print("Set language: Spanish")
+        if fw903plus:
+            ih.puts(date_addr, DATE_SPA)
+            changed = True
+        elif (month_addr := ih.find(MONTH_SPA)) != -1:
+            ih.puts(date_addr, DAY_SPA)
+            changed = True
+
+    if args['ger'] and lang:
+        print("Set language: German")
+        if fw903plus:
+            ih.puts(date_addr, DATE_GER)
+            changed = True
+        elif (month_addr := ih.find(MONTH_GER)) != -1:
+            ih.puts(date_addr, DAY_GER)
+            changed = True
+
+    if args['ita'] and lang:
+        print("Set language: Italian")
+        if fw903plus:
+            ih.puts(date_addr, DATE_ITA)
+            changed = True
+        elif (month_addr := ih.find(MONTH_ITA)) != -1:
+            ih.puts(date_addr, DAY_ITA)
+            changed = True
+
+    if args['por'] and lang:
+        print("Set language: Portuguese")
+        if fw903plus:
+            ih.puts(date_addr, DATE_POR)
+            changed = True
+        elif (month_addr := ih.find(MONTH_POR)) != -1:
+            ih.puts(date_addr, DAY_POR)
+            changed = True
+    if not changed:
+        print("Error: Date strings not found")
+
+if args['bmpfile']:
     filename = args['bmpfile']
 
     try:
