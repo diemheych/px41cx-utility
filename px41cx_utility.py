a5b5293391876ca4fecf44f6d90c9ef1c2a6d0a5
diemheych <100515426+diemheych@users.noreply.github.com>
2025-02-19 09:11:29 +1100

    Update px41cx_utility.py
    Missing os module import

--- px41cx_utility.py
+++ px41cx_utility.py
@@ -58,6 +58,7 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <https://www.gnu.org/licenses/>.
 #
+import os
 import argparse
 from intelhex import IntelHex
 
